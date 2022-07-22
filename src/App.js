d47ae23d125bc22299b9b369e689ddfd258ff3fb
sparashuram <sparashu71@gmail.com>
2022-07-22 14:13:50 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,46 +1,42 @@
-import React,{useEffect,useState} from 'react';
-import axios from 'axios';
-import Gallery from './Gallery';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import React,{useState} from 'react';
+import TodoList from './TodoList';
 
-const apiKey = "636e1481b4f3c446d26b8eb6ebfe7127";
 const App = () => {
-  const [data,setData] = useState([]);
-  const [search,setSearch] = useState("");
-  useEffect(()=>{
-    },[])
-  const changeHandler = e =>{
-    setSearch(e.target.value);
+  const [task,SetTask]=useState("")
+  const[todos,setTodos]=useState([])
+  const changeHandler=e =>{
+    SetTask([e.target.value])
   }
-  const submitHandler = e =>{
+  const submitHandler=e =>{
     e.preventDefault();
-    axios
-    .get(
-      `https://api.flickr.com/services/rest/?method=flickr.photos.search&api_key=${apiKey}&tags=${search}&per_page=24&format=json&nojsoncallback=1`
-    )
-    .then(response => {
-      setData(response.data.photos.photo)
-    })
-    .catch(error => {
-      console.log(
-        "Encountered an error with fetching and parsing data",
-        error
-      );
-  })
+    const newTodos=[...todos,task]
+    setTodos(newTodos)
+    SetTask("")
+  }
+  const deleteHandler=(indexValue) =>{
+  const newTodos=todos.filter((todo,index) =>index!==indexValue);
+  setTodos(newTodos);
   }
   return (
     <div>
       <center>
-        <h2>Gallery Snapshot</h2><br></br>
+    <div className='card'>
+      <div className='card-body'>
+        <h1 className="card-title">
+          Todo management application
+        </h1>
         <form onSubmit={submitHandler}>
-          <input size="30" type="text" onChange={changeHandler} value={search}/><br /><br />
-          <input type="submit" name="Search" />
+          <input type="text"  name="task" value={task} onChange={changeHandler}></input>&nbsp;&nbsp;
+          <input type="submit"name='Add'value="Add"></input>
         </form>
-        <br />
-        {data.length>=1?<Gallery data={data}/>:<h4>No Image Loaded</h4>}
-      </center>
+        <TodoList todos={todos} deleteHandler={deleteHandler}/>
+
+      </div>
+      
     </div>
-  )
+    </center>
+    </div>
+  );
 }
 
-export default App
+export default App;
